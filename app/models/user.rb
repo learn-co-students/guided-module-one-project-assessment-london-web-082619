f7c95476fd60146739a6c21b31b981e2aca81f68
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-09 14:13:03 +0100

    Initialised main menu and search menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base 
     has_many :tickets
     has_many :events, through: :tickets
+
+    def view_tickets
+        #Create array of strings which has ticket info
+        # self.tickets
+        # prompt = TTY::Prompt.new
+        # prompt.select("", )
+    
+    end
+
+
 end 
 
 
