3a4c8c90c782495314401a704743542744715d61
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-09 11:04:21 +0100

    api communicator small update

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -11,17 +11,19 @@ require 'pry'
 # data = RestClient.get("https://www.eventbriteapi.com/v3/users/me", headers = {Authorization: Bearer "CXS22JWMRNKAGSTG6RXF"} )
 
 event_search = "https://www.eventbriteapi.com/v3/events/search"
+categories = "https://www.eventbriteapi.com/v3/categories"
 token = "/?token=CXS22JWMRNKAGSTG6RXF"
 user = "https://www.eventbriteapi.com/v3/users/me"
 event = "https://www.eventbriteapi.com/v3/events/search?location.address=vancovuer&location.within=10km&expand=venue"
 
 def get_api_response(url)
     response_string = RestClient.get(url)
-    data = JSON.parse(response_string)
+    data = JSON.padatarse(response_string)
 end
 
 data = get_api_response(user+token)
 data2 = get_api_response(event_search+token)
+data3 = get_api_response(categories+token)
 # events = data2["events"]
 
 binding.pry
